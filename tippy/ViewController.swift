daa0b0a3324b82726d59b22df68f2c8d1fcf10b4
Kate Odnous <kate.odnous@airbnb.com>
2016-07-17 21:33:26 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by kate_odnous on 7/17/16.
+//  Copyright © 2016 Kate Odnous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
